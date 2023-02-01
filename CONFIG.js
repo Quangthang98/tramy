ae4208f60beb353aa78159a3eb6cbe431df3f3c7
Quangthang98 <124145745+Quangthang98@users.noreply.github.com>
2023-02-01 18:07:02 +0700

    Initial commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Tiếp tục làm bồ của THANGLE mãi mãi nha!!!!",
+  descriptionHeader:
+    "Nếu cậu thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! để suy nghỉ lại cái.",
+  titleModar: "Cảm ơn em vì đồng ý",
+  descriptionModar:
+    'Cảm ơn Em vì đã tiếp tục làm người yêu của Anh ^^ </br> Anh không hứa gì cả những sẽ cố gắng để Anh và Em có thể đi trên con đường tình yêu này dài hết mức có thể:)) </br> Anh yêu Em </br> Mãi yêu bồ ,
+};
